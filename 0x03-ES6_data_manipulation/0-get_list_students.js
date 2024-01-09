9c61565dc73f3e49a102e4a8b00ab9cffbafedf3
Femzysamie <femzysamiejebty@gmail.com>
2024-01-09 08:54:49 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,12 @@
-function getListStudents() {
+/**
+ * Retrieves a list of students.
+ * @author Jebutu Oluwafemi <https://github.com/femzysamie>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
